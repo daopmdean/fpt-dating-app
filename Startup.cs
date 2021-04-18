c7e852d83bfb5d76d6eccc3023df5e6b1edd0c8b
Dao Pham <daopm.dean@gmail.com>
2021-04-18 11:58:26 +0700

    update startup.cs

--- Startup.cs
+++ Startup.cs
@@ -127,11 +127,7 @@ namespace DatingApp.API
             app.UseCors(x => x
                 .AllowAnyMethod()
                 .AllowAnyHeader()
-                .WithOrigins("http://localhost:4200/")
-                //.SetIsOriginAllowed(origin => true) // allow any origin
-                .AllowCredentials()); // allow credentials);
-
-
+                .AllowAnyOrigin()); // allow credentials);
 
             app.UseRouting();
 
